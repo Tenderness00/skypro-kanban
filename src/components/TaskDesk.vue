8b892c49be0397e0ae117bab11e14d40013017ec
Tenderness00 <sytovaalina00@gmail.com>
2025-04-05 17:38:48 +0700

    исправление ошибок

---
+++ src/components/TaskDesk.vue
@@ -0,0 +1,17 @@
+<script setup>
+import VColumn from './VColumn.vue';
+
+</script>
+
+<template>
+    <div class="container main__content">
+        <VColumn title="Без статуса" />
+        <VColumn title="Нужно сделать" />
+        <VColumn title="В работе" />
+        <VColumn title="Тестирование" />
+        <VColumn title="Готово" />
+
+    </div>
+        
+    </template>
+    
---
+++ src/components/VColumn.vue
@@ -0,0 +1,17 @@
+<script setup>
+import VTask from './VTask.vue';
+defineProps({title: {type: String, required: true}})
+</script>
+
+<template>
+<div class="main__column">
+    <div class="column__title">
+        <p>{{ title }}</p>
+    </div>
+    <div class="cards">
+        <VTask/>
+        <VTask/>
+        <VTask/>
+    </div> 
+</div>
+</template>
